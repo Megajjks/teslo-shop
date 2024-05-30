3857d8a1ac0775aab7d6d78a5f77d2a0809d0794
Jayro Salazar <megajjks@gmail.com>
2024-05-29 19:00:08 -0600

    create dtos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true, //remueve los parametros que no se modifican o usan
+      forbidNonWhitelisted:true, // te trata como error los parametros que no estan definidos en el DTO
+      transform:true, // activate tranforms dto value to income
+      transformOptions: {
+        enableImplicitConversion:true,
+      }
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
