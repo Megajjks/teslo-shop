8818dfd1ad0f9c9196d79254b05a258cc7b886a6
Jayro Salazar <megajjks@gmail.com>
2024-08-05 14:44:47 -0600

    share auth module

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,12 +4,15 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Auth(ValidRoles.user)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -25,11 +28,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.user)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  //@Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from 'src/products/products.module';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
   imports:[
-    ProductsModule
+    ProductsModule,
+    AuthModule
   ]
 })
 export class SeedModule {}
